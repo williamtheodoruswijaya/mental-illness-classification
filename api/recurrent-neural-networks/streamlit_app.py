c4bee8eace4627c8e48d19aec8da225e488442fd
Admantix <williamwijaya499@gmail.com>
2025-04-25 23:47:56 +0700

    Fix file path for loading tokenizer in Streamlit app

--- api/recurrent-neural-networks/streamlit_app.py
+++ api/recurrent-neural-networks/streamlit_app.py
@@ -16,7 +16,7 @@ file_path = os.path.join(os.path.dirname(__file__), 'RNNModel.keras')
 model = tf.keras.models.load_model(file_path)
 
 # Load tokenizer
-with open('tokenizer.pkl', 'rb') as handle:
+with open(os.path.join(os.path.dirname(__file__), 'tokenizer.pkl'), 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 label_ordered = {
